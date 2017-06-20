b26efa39ed1f134501ca7e30e4e396a3ad0e8c8c
mirzanove <mirzanove@gmail.com>
2017-06-20 18:46:55 +0300

    fff

--- clientscript/settings.js
+++ clientscript/settings.js
@@ -390,7 +390,7 @@ jQueryM_v1_4_5("#wrapper").scroll( function() {
 } ////////////////////////////////////////////////////////////////
 else {
 	jQueryD_1_4_2(document).ready(function() {
-		alert("coool");
+		alert("coool2");
 		
 		  //var span = document.getElementsByClassName("close")[0];
 
