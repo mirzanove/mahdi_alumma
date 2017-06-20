d300f2bc74459eb20c8303962a9d973249c07561
mirzanove <mirzanove@gmail.com>
2017-06-20 19:18:35 +0300

    gggg

--- template/scripts/mhfhost.js
+++ template/scripts/mhfhost.js
@@ -83,13 +83,8 @@ function doSearch()
 	var searchText = rh.model.get(rh.consts('KEY_SEARCH_TERM'));
 	
 	if(document.getElementById("checkbox_id1").checked)
-	{          alert("maaaaaaaaaaatch3");
-	           var isquot = /^\"/g.test(searchText) && /\"$/g.test(searchText);
-	           if(isquot === true){
-	           searchText = '"'+searchText.replace(/["']/g, "")+'"';}
-	           else{
-	           searchText =  '"'+searchText+'"'; 
-		       }
+	{          alert("maaaaaaaaaaatch15");
+	          
 	  
 	        if(searchText) {
 		       rh.model.publish(rh.consts('KEY_SEARCHED_TERM'), searchText, {sync: true});
