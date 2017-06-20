d46ebad7a96c49d6d1b6f5169f970a2701820902
mirzanove <mirzanove@gmail.com>
2017-06-20 19:49:04 +0300

    jjjj

--- template/scripts/mhfhost.js
+++ template/scripts/mhfhost.js
@@ -83,25 +83,14 @@ function doSearch()
 	var searchText = rh.model.get(rh.consts('KEY_SEARCH_TERM'));
 	
 	if(document.getElementById("checkbox_id1").checked)
-	{          //alert("maaaaaaaaaaatch17");
+	{        
 	           
 	  
 	        if(searchText) {
 		     
-			   var isquot =  /^".*"$/g.test(searchText);
-	           if(isquot === true){
 	           searchText = '"'+searchText.replace(/["']/g, "")+'"';
-			    alert(searchText+"hhhh");
+			    alert(searchText+"lllllllllllllll");
 			   rh.model.publish(rh.consts('KEY_SEARCHED_TERM'), searchText, {sync: true});
-			   }
-	           else{
-	           searchText = '"'+searchText.replace(/["']/g, "")+'"';
-			   
-			   
-			   alert(searchText+"jjjj");
-			   rh.model.publish(rh.consts('KEY_SEARCHED_TERM'), searchText, {sync: true});
-		       }
-
 		       rh.model.publish(rh.consts('EVT_SEARCH_IN_PROGRESS'), true, {sync: true});
 		       rh.model.publish(rh.consts('KEY_SEARCH_PROGRESS'), 0, {sync: true});
 		       initSearchPage();
