2fa82c85f6d1ed060c6c44d092b67149bf8800ce
mirzanove <mirzanove@gmail.com>
2017-06-26 18:13:00 -0700

    gdfdgdf

--- clientscript/settings.js
+++ clientscript/settings.js
@@ -156,7 +156,7 @@ jQueryM_v1_4_5("#wrapper").scroll( function() {
 			 if((classname != "date")&&(classname != "link_org")&&(classname != "imglink")&&(classname != "hide_external_link")&&(classname != "tooltiptext")&&(classname != "alpom")&&(classname != "pda tooltip4")&&(classname != "pda tooltip4 org")&&(classname != "largefont tooltip3")&&(classname != "largefont tooltip2")&&(classname != "username")&&(classname != "bbcode_link")){
 			 
 			 //alert(link);
-			     if((link.indexOf("file://") == 0)||(link.indexOf("http://localhost") == 0)||(link.indexOf("https://mirzanove.github.io/") == 0)||(link.indexOf("http://bayan-noon.com") == 0)) {
+			     if((link.indexOf("file://") == 0)||(link.indexOf("http://localhost") == 0)||(link.indexOf("https://mirzanove.github.io/") == 0)||(link.indexOf("http://bayan-noon.com") == 0)||(link.indexOf("https://bayan-noon.com") == 0)) {
                         document.getElementsByClassName("loading")[0].style.display = 'block';
 						//alert(link.indexOf("file://"));
 						//alert("cool");
@@ -174,7 +174,7 @@ jQueryM_v1_4_5("#wrapper").scroll( function() {
 				location.href = link;
                 return false;
              }
-            else if((link.indexOf("file://") == 0)||(link.indexOf("http://localhost/") == 0)||(link.indexOf("https://mirzanove.github.io/") == 0)||(link.indexOf("http://bayan-noon.com") == 0)){
+            else if((link.indexOf("file://") == 0)||(link.indexOf("http://localhost/") == 0)||(link.indexOf("https://mirzanove.github.io/") == 0)||(link.indexOf("http://bayan-noon.com") == 0)||(link.indexOf("https://bayan-noon.com") == 0)){
 				 
 			 if(checkURL(link) == true){
 				  if(classname == "imglink"){
@@ -419,7 +419,7 @@ else {
 			 if((classname != "date")&&(classname != "link_org")&&(classname != "imglink")&&(classname != "hide_external_link")&&(classname != "tooltiptext")&&(classname != "alpom")&&(classname != "pda tooltip4")&&(classname != "pda tooltip4 org")&&(classname != "largefont tooltip3")&&(classname != "largefont tooltip2")&&(classname != "username")&&(classname != "bbcode_link")){
 			 
 			 //alert(link);
-			     if((link.indexOf("file://") == 0)||(link.indexOf("http://localhost/") == 0)||(link.indexOf("https://mirzanove.github.io/") == 0)||(link.indexOf("http://bayan-noon.com") == 0)) {
+			     if((link.indexOf("file://") == 0)||(link.indexOf("http://localhost/") == 0)||(link.indexOf("https://mirzanove.github.io/") == 0)||(link.indexOf("http://bayan-noon.com") == 0)||(link.indexOf("https://bayan-noon.com") == 0)) {
                         document.getElementsByClassName("loading")[0].style.display = 'block';
 						//alert(link.indexOf("file://"));
 						//alert("cool");
@@ -438,7 +438,7 @@ else {
                 return false;
 				
              }
-            else if((link.indexOf("file://") == 0)||(link.indexOf("http://localhost/") == 0)||(link.indexOf("https://mirzanove.github.io/") == 0)||(link.indexOf("http://bayan-noon.com") == 0)){
+            else if((link.indexOf("file://") == 0)||(link.indexOf("http://localhost/") == 0)||(link.indexOf("https://mirzanove.github.io/") == 0)||(link.indexOf("http://bayan-noon.com") == 0)||(link.indexOf("https://bayan-noon.com") == 0)){
 				 
 			 if(checkURL(link) == true){
 				  if(classname == "imglink"){
