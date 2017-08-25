1488e8fdca07b89ec24fe83f3e1c21c062062d1d
mirzanove <mirzanove@gmail.com>
2017-08-25 16:28:31 +0300

    dfddg

--- whxdata/toc1.new.js
+++
@@ -1,4 +0,0 @@
-(function() {
-var toc =  [{"type":"book","name":"forum1","key":"toc2"},{"type":"book","name":"forum2","key":"toc3"},{"type":"book","name":"forum4","key":"toc4"},{"type":"book","name":"forum5","key":"toc5"}];
-window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
-})();
