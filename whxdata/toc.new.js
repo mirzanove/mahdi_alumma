d390e0946f7d5390cd4b07222d07ccdfe11f3e8e
mirzanove <mirzanove@gmail.com>
2017-09-02 15:31:00 +0300

    new

---
+++ whxdata/toc.new.js
@@ -0,0 +1,4 @@
+(function() {
+var toc =  [{"type":"item","name":"المنتدى","url":"index.php.htm"},{"type":"book","name":"index.php","key":"toc1"}];
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
+})();
