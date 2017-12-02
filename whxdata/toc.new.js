98e5e677321c5c0077a26ea9c78ed82ffcdd2c3a
mirzanove <mirzanove@gmail.com>
2017-12-02 18:25:26 +0200

    clean

--- whxdata/toc.new.js
+++
@@ -1,4 +0,0 @@
-(function() {
-var toc =  [{"type":"item","name":"المنتدى","url":"index.php.htm"},{"type":"book","name":"index.php","key":"toc1"}];
-window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
-})();
