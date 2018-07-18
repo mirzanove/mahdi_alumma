2b39bc52a0b7bb223a2bbe34983c2a14aafac8c3
mirzanove <mirzanove@gmail.com>
2018-07-18 03:02:29 +0300

    format

--- whxdata/toc1.new.js
+++
@@ -1,4 +0,0 @@
-(function() {
-var toc =  [{"type":"book","name":"forum1","key":"toc2"},{"type":"book","name":"forum2","key":"toc3"},{"type":"book","name":"forum4","key":"toc4"},{"type":"book","name":"forum5","key":"toc5"},{"type":"book","name":"forum6","key":"toc6"},{"type":"book","name":"forum7","key":"toc7"}];
-window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
-})();
