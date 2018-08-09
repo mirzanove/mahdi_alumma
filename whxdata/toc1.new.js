1983d79880a8605343081765a8e38e277fe2e9a5
mirzanove <mirzanove@gmail.com>
2018-08-10 01:21:28 +0300

    2018

---
+++ whxdata/toc1.new.js
@@ -0,0 +1,4 @@
+(function() {
+var toc =  [{"type":"book","name":"forum1","key":"toc2"},{"type":"book","name":"forum10","key":"toc3"},{"type":"book","name":"forum2","key":"toc4"},{"type":"book","name":"forum4","key":"toc5"},{"type":"book","name":"forum5","key":"toc6"},{"type":"book","name":"forum6","key":"toc7"},{"type":"book","name":"forum7","key":"toc8"},{"type":"book","name":"forum8","key":"toc9"},{"type":"book","name":"forum9","key":"toc10"}];
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
+})();
---
+++ whxdata/toc3.new.js
@@ -0,0 +1,4 @@
+(function() {
+var toc =  [{"type":"item","name":"? البيانات الموجودة في الاقسام الخاصة ?","url":"index.php/forum10/f-10.html"}];
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
+})();
