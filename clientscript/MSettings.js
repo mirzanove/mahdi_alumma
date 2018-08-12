6c48b9e4d0040b47f9b6cb9d3f7da1b56137ba8a
mirzanove <mirzanove@gmail.com>
2018-08-13 01:08:15 +0300

    jjjjjjjjjjj

--- clientscript/MSettings.js
+++ clientscript/MSettings.js
@@ -558,7 +558,7 @@ jQueryM_v1_4_5("#content").append('<div class="selecth1FontFamily" >تغيير �
 						 jQueryM_v1_4_5('#content ul  > li').css("font-size","90%");
 						 jQueryM_v1_4_5('#navbar').css("font-size","77%");
 						 jQueryM_v1_4_5('.thread_title').css("font-size","85%");
-						 jQueryM_v1_4_5('.dropdown-content').css("font-size","70%");
+						 //jQueryM_v1_4_5('.dropdown-content').css("font-size","70%");
 						 
 						 
 		             }
@@ -568,7 +568,7 @@ jQueryM_v1_4_5("#content").append('<div class="selecth1FontFamily" >تغيير �
 						 jQueryM_v1_4_5('#content ul  > li').css("font-size","80%");
 						 jQueryM_v1_4_5('#navbar').css("font-size","65%");
 						 jQueryM_v1_4_5('.thread_title').css("font-size","70%");
-						 jQueryM_v1_4_5('.dropdown-content').css("font-size","60%");
+						 //jQueryM_v1_4_5('.dropdown-content').css("font-size","60%");
 		             }
 					 
 				 }
