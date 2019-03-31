8d03708f488254a4f97241f66c1f5e3dce39d232
mirzanove <mirzanove@gmail.com>
2019-03-31 18:26:33 +0300

    ---------------------------

---
+++ www/whxdata/toc2.js
@@ -0,0 +1,1 @@
+gXMLBuffer ="<?xml version=\"1.0\" encoding=\"utf-8\" ?><data  src=\"toc1.js\"  name=\"forum1\"><item name=\"? ãÌãæÚÉ ÈíÇäÇÊ ÇáÅãÇã ÇáãåÏí äÇÕÑ ãÍãÏ ÇáíãÇäí ?\" url=\"index.php/forum1/f-1.html\" /></data>";
---
+++ www/whxdata/toc2.new.js
@@ -0,0 +1,4 @@
+(function() {
+var toc =  [{"type":"item","name":"? ãÌãæÚÉ ÈíÇäÇÊ ÇáÅãÇã ÇáãåÏí äÇÕÑ ãÍãÏ ÇáíãÇäí ?","url":"index.php/forum1/f-1.html"}];
+window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
+})();
