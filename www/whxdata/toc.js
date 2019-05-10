026621f2fa9936ee815a94df6e66a4d85a57efe6
mirzanove <mirzanove@gmail.com>
2019-05-11 00:45:43 +0300

    -----------------------

--- www/whxdata/toc.js
+++
@@ -1,1 +0,0 @@
-gXMLBuffer ="<?xml version=\"1.0\" encoding=\"utf-8\" ?><data ><item name=\"help\" url=\"help.html\" /><book name=\"index.php\" src=\"toc1.js\" /><item name=\"ÇáãäÊÏì\" url=\"index.php.htm\" /></data>";
--- www/whxdata/toc2.new.js
+++
@@ -1,4 +0,0 @@
-(function() {
-var toc =  [{"type":"item","name":"? ãÌãæÚÉ ÈíÇäÇÊ ÇáÅãÇã ÇáãåÏí äÇÕÑ ãÍãÏ ÇáíãÇäí ?","url":"index.php/forum1/f-1.html"}];
-window.rh.model.publish(rh.consts('KEY_TEMP_DATA'), toc, { sync:true });
-})();
